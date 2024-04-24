759f8e1e24dcf7140f6fedc7eff946aa06fe2f3a
Victor Castro <vicastro@pdx.edu>
2024-04-24 14:00:17 -0700

    Hw2 part 5 done

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,32 @@
 // Add your code here
+document.getElementById("form").addEventListener("submit", handleInput);
+const modal = document.getElementById("form-content");
+const form = document.getElementById("form");
+
+// Returns key pair values
+function getData(form) {
+  return new FormData(form);
+}
+
+// Loop through and append to modal, create a p element
+function addData(data) {
+  console.log(data);
+  modal.innerHTML = "";
+
+  for (let [key, value] of data.entries()) {
+    const newData = document.createElement("div");
+    newData.textContent = `For ${key}, you have entered: ${value}`;
+    modal.appendChild(newData);
+  }
+}
+
+// The Bootstrap 5 supports js
+function handleInput(event) {
+  const toShow = new bootstrap.Modal(document.getElementById("show"));
+
+  // Default is reloading the page, but we need to show modal
+  event.preventDefault();
+  const formData = getData(form);
+  addData(formData);
+  toShow.show();
+}
